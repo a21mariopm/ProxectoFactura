bfe6578db1ad24e3f4c7ae4b895e036043541a3e
a21mariopm <a21mariopm@a13eq15.sanclemente.local>
2022-03-15 10:05:20 +0100

    actualizacion

--- ProxectoFactura/src/main/java/com/mycompany/proxectofactura/ArtigoFactura.java
+++ ProxectoFactura/src/main/java/com/mycompany/proxectofactura/ArtigoFactura.java
@@ -1,7 +1,7 @@
 package com.mycompany.proxectofactura;
 
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * ---->Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 
